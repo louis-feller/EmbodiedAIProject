631fa30212f44eb998e45dd8d3fcbd94fdacb283
willimaki <condowilliam@outlook.fr>
2023-11-01 16:40:49 +0100

    ajout controle pince

--- tests.py
+++ tests.py
@@ -1,29 +1,47 @@
 from ev3dev.auto import *
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B,OUTPUT_C,OUTPUT_D, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 import time
 
-A = Motor(OUTPUT_A) #wheel
-B = Motor(OUTPUT_B)
-C = Motor(OUTPUT_C)
-D = Motor(OUTPUT_D) #wheel
+mA = OUTPUT_A #wheel
+mB = Motor(OUTPUT_B)
+mC = Motor(OUTPUT_C)
+mD = OUTPUT_D #wheel
 
 ts = TouchSensor()
+ls = LightSensor()
 cs = ColorSensor()
 us = UltrasonicSensor()
 leds = Leds()
 
 # Motors Control
-roues = MoveTank(A,D)
-roues.on_for_seconds(40,40,5)
+roues = MoveTank(mA,mD)
+#roues.on_for_seconds(-100,-100,3.5)
+
+
+#Test light/Color sensor
+while(True):
+#test grap
+# mC.on_for_seconds(SpeedPercent(-40),3)
+''''
+3sec pour ouvrir et fermer
+'''
+#mC.run_timed(time_sp=3000, speed_sp=700)
+
+# Ultrasonic sensor
+#while(True):
+#    print(us.distance_centimeters)
+
+
+
 
 '''
 while True:
-    if ts.is_pressed:
+    if ts.is_pressed: 
         leds.set_color("LEFT", "GREEN")
         leds.set_color("RIGHT", "GREEN")
     else:
@@ -32,14 +50,14 @@ while True:
     # don't let this loop use 100% CPU
     sleep(0.010)
     m = Motor(OUTPUT_B)
-    m.run_timed(time_sp=1501, speed_sp=-500) #negatif = fermeture
+    m.run_timed(time_sp=1501, speed_sp=-500) #negatif = fermetures
 
 while not ts.is_pressed:
     #A.run_timed(speed_sp=-100)
     #D.run_timed(speed_sp=-100)
     us.MODE_US_DIST_CM
     print(us.distance_centimeters)
-"""
+'''
 
 sleep(2)
 #B.run_timed(time_sp=2000,speed_sp=-500)  # negatif = fermeture
