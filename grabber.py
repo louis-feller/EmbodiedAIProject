170ff11ae9b726610e4fcf54dfa81fafb61aed4a
Louis-Feller <lofel23@student.sdu.dk>
2023-11-15 18:50:09 +0100

    updatded grabber without touch sensor

--- grabber.py
+++ grabber.py
@@ -2,35 +2,19 @@ from ev3dev.auto import *
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 
-MAX_CLOSE = -3000
-MAX_OPEN = 3000
+TRAVEL_TIME = 5000
 CLOSING_SPEED = -700
 OPENING_SPEED = -CLOSING_SPEED
 
 class Grabber :
-    def __init__(self, motor, touch_sensor) -> None:
-        self._pos = 0
+    def __init__(self, motor) -> None:
         self._motor = motor
-        self._touch = touch_sensor
         
-    '''Closes the grabber as to grab the can
-    Returns the time in ms it took to close'''
+    '''Closes the grabber'''
     def close(self) :
-        while (not self._touch.is_pressed) and (self._pos >= MAX_CLOSE) :
-            self._motor.run_timed(time_sp=10, speed_sp=CLOSING_SPEED) #negative speed for closing
-            self._pos -= 10
-        if self._pos != MAX_CLOSE :
-            self._motor.run_timed(time_sp= 280, speed_sp= CLOSING_SPEED)
-            self._pos -= 280
+            self._motor.run_timed(time_sp=TRAVEL_TIME, speed_sp=CLOSING_SPEED) #negative speed for closing
+            
 
-    '''Open the grabber for t_ms milliseconds'''    
+    '''Open the grabber'''    
     def open(self) :
-        self._motor.run_timed(time_sp=MAX_OPEN - self._pos, speed_sp = OPENING_SPEED) #positive speed for opening
-        self._pos = MAX_OPEN
-
-    def reset(self) :
-        if self._pos > 0 : #grabber in rather open pos
-            sp = CLOSING_SPEED
-        else :
-            sp = OPENING_SPEED
-        self._motor.run_timed(time_sp=abs(self._pos), speed_sp = sp)
+        self._motor.run_timed(time_sp=TRAVEL_TIME, speed_sp = OPENING_SPEED) #positive speed for opening
