96fbcfbee284283cd9ec56ca35da814dbb5837af
aqzsedrftgyhujikol <148978144+aqzsedrftgyhujikol@users.noreply.github.com>
2023-11-15 14:00:34 +0100

    Add files via upload

--- behaviors.py
+++ behaviors.py
@@ -14,6 +14,95 @@ grabber = Grabber(motor=grab_motor, touch_sensor=ts)
 - run into can
 - close gripper
 - track backwards to line (hopefully)'''
+def line():
+    gs.reset()
+    # parametres nuit :
+    kp = 3.3 * 100
+    ki = 2.0 * 100
+    kd = 1.9 * 100
+    Tp = -20
+
+    # Calibrer avant !!!
+    offset = 12.0
+    integral = 0
+    derivative = 0.0
+    lastError = 0
+
+    roues = MoveTank(mA, mD)
+
+
+    while (True):
+        while gs.angle > 1000.0:
+            # roues.sleep_time(0.010)
+            Tp += -6
+        i=0
+        while i<3:
+            i+=1
+        light = cs.reflected_light_intensity
+        error = light - offset
+        print("Error : ", error)
+        derivative = error - lastError
+        Turn = (kp * error + ki * integral + kd * derivative) / 100
+        # print("Turn :", Turn)
+        powerLw = Tp - Turn
+        powerRw = Tp + Turn
+        lastError = error
+        # print("L :", powerLw, " /  R : ", powerRw)
+        roues.on(clamp(powerLw, -100, 100), clamp(powerRw, -100, 100))
+        return (powerLw,powerRw)
+        # sleep(2)
+
+
+def detect_can():
+    #cpt2=0
+    #roues.on(clamp(-powerLw, -100, 100), clamp(-powerRw, -100, 100))
+    #while(cpt2<=10):
+        #roues.on(clamp(-powerLw, -100, 100), clamp(-powerRw, -100, 100))
+        #cpt2=1
+    mA=Motor(OUTPUT_A)
+    mD=Motor(OUTPUT_D)
+    mA.run_timed(time_sp=1500, speed_sp=-300)
+    mD.run_timed(time_sp=1500, speed_sp=-300)
+    sleep(0.010)
+    timemin=0
+    min=us.distance_centimeters
+    time1=0
+    while time1<=5000:
+        mD.run_timed(time_sp=100, speed_sp=-100)
+        time1+=100
+        if us.distance_centimeters<min:
+            min=us.distance_centimeters
+            timemin=time1
+    sleep(0.010)
+    mD.run_timed(time_sp=5000, speed_sp=100)
+    sleep(0.010)
+    time2=0
+    while time2>-5000:
+        mA.run_timed(time_sp=100, speed_sp=-100)
+        time2-=100
+        if us.distance_centimeters<min:
+            min=us.distance_centimeters
+            timemin=time2
+    sleep(0.010)
+    if timemin<=0:
+        mA.run_timed(time_sp=5000+timemin, speed_sp=100)
+    else:
+        mA.run_timed(time_sp=5000, speed_sp=100)
+        sleep(0.010)
+        mD.run_timed(time_sp=timemin, speed_up=-100)
+    grab_can()
+    
+
+    
+    
+
+cpt1=0
+l,r=line()
+while(cpt1<=10 and l>r):
+    l,r=line()
+    cpt1=1
+detect_can()
+
 def grab_can() :
     sp = SpeedPercent(20)
     grabber.open()
