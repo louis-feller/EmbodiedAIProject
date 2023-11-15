e7a57210ca5b01fb0c0683d643837687b864b4a6
willimaki <condowilliam@outlook.fr>
2023-11-15 23:12:11 +0100

    detect 2 ok

--- behaviors.py
+++ behaviors.py
@@ -16,7 +16,7 @@ mC = Motor(OUTPUT_C)
 mD = OUTPUT_D  # wheel
 roues = MoveTank(mA, mD)
 
-#ts = TouchSensor()
+# ts = TouchSensor()
 cs = ColorSensor()
 spkr = Sound()
 gs = GyroSensor()
@@ -30,6 +30,8 @@ grabber = Grabber(mC)
 - run into can
 - close gripper
 - track backwards to line (hopefully)'''
+
+
 def line():
     gs.reset()
     # parametres nuit :
@@ -46,14 +48,13 @@ def line():
 
     roues = MoveTank(mA, mD)
 
-
     while (True):
         while gs.angle > 1000.0:
             # roues.sleep_time(0.010)
             Tp += -6
-        i=0
-        while i<3:
-            i+=1
+        i = 0
+        while i < 3:
+            i += 1
         light = cs.reflected_light_intensity
         error = light - offset
         print("Error : ", error)
@@ -65,74 +66,120 @@ def line():
         lastError = error
         # print("L :", powerLw, " /  R : ", powerRw)
         roues.on(clamp(powerLw, -100, 100), clamp(powerRw, -100, 100))
-        return (powerLw,powerRw)
+        return (powerLw, powerRw)
         # sleep(2)
 
 
-def detect_can():
-    #cpt2=0
-    #roues.on(clamp(-powerLw, -100, 100), clamp(-powerRw, -100, 100))
-    #while(cpt2<=10):
-        #roues.on(clamp(-powerLw, -100, 100), clamp(-powerRw, -100, 100))
-        #cpt2=1
-    mA=Motor(OUTPUT_A)
-    mD=Motor(OUTPUT_D)
-    mA.run_timed(time_sp=1200, speed_sp=-300)
-    mD.run_timed(time_sp=1200, speed_sp=-300)
-    sleep(1.5)
-    timemin=0
-    min=us.distance_centimeters
-    time1=0
-    while time1<=10000:
+def detect_can(grabber):
+    # cpt2=0
+    # roues.on(clamp(-powerLw, -100, 100), clamp(-powerRw, -100, 100))
+    # while(cpt2<=10):
+    # roues.on(clamp(-powerLw, -100, 100), clamp(-powerRw, -100, 100))
+    mA = Motor(OUTPUT_A)
+    mD = Motor(OUTPUT_D)
+    # cpt2=1
+    spkr.beep()
+    timemin = 0
+    min = us.distance_centimeters
+    time1 = 0
+    while time1 <= 10000:
         mD.run_timed(time_sp=100, speed_sp=-100)
         sleep(0.1)
-        time1+=100
-        if us.distance_centimeters<min:
-            min=us.distance_centimeters
-            timemin=time1
-    mD.run_timed(time_sp=10000, speed_sp=100)
-    sleep(10)
-    time2=0
-    while time2>-10000:
+        time1 += 100
+        if us.distance_centimeters < min:
+            min = us.distance_centimeters
+            timemin = time1
+    mD.run_timed(time_sp=4000, speed_sp=100)
+    sleep(4)
+    time2 = 0
+    while time2 > -6000:
         mA.run_timed(time_sp=100, speed_sp=-100)
         sleep(0.1)
-        time2-=100
-        if us.distance_centimeters<min:
-            min=us.distance_centimeters
-            timemin=time2
-    if timemin<=0:
-        mA.run_timed(time_sp=10000+timemin, speed_sp=100)
-        sleep(10+timemin/1000)
+        time2 -= 100
+        if us.distance_centimeters < min:
+            min = us.distance_centimeters
+            timemin = time2
+    print('Timemin :', timemin)
+    if timemin <= 0:
+        mA.run_timed(time_sp=(10000 + timemin) / 2.5, speed_sp=100)
+        sleep((10 + timemin / 1000)/2.5)
     else:
-        mA.run_timed(time_sp=10000, speed_sp=100)
-        sleep(10)
-        mD.run_timed(time_sp=timemin, speed_sp=-100)
-        sleep(timemin/1000)
-    while us.distance_centimeters>3.5:
+        mA.run_timed(time_sp=4000, speed_sp=100)
+        sleep(4)
+        mD.run_timed(time_sp=timemin / 2.63, speed_sp=-100)
+        sleep(timemin / 1000)
+    while us.distance_centimeters >= 4.5:
         mA.run_timed(time_sp=1000, speed_sp=-200)
         mD.run_timed(time_sp=1000, speed_sp=-200)
- 
-#cpt1=0
-#l,r=line()
-# while(cpt1<=10 and l>r):
-#     l,r=line()
-#     cpt1=1
-detect_can()
+        print("Distance : ", us.distance_centimeters)
+    grabber.down()
 
-grabber.open()
-grabber.close()
+def detect_can2():
+    mA = Motor(OUTPUT_A)
+    mD = Motor(OUTPUT_D)
 
+    spkr.beep()
 
+    timemin = 0
+    min_distance = us.distance_centimeters  # Store the minimum distance
+    time1 = 0
+
+    # Move the robot forward and update the minimum distance
+    while time1 <= 10000:
+        mD.run_timed(time_sp=100, speed_sp=-100)
+        sleep(0.1)
+        time1 += 100
+        current_distance = us.distance_centimeters
+        if current_distance < min_distance:
+            min_distance = current_distance
+            timemin = time1
+
+    # Move the robot backward and update the minimum distance
+    mD.run_timed(time_sp=4000, speed_sp=100)
+    sleep(4)
+    time2 = 0
+    while time2 > -10000:
+        mA.run_timed(time_sp=100, speed_sp=-100)
+        sleep(0.1)
+        time2 -= 100
+        current_distance = us.distance_centimeters
+        if current_distance < min_distance:
+            min_distance = current_distance
+            timemin = time2
+
+    print('Timemin :', timemin)
+
+    # Move the robot back to the position of minimum distance
+    if timemin <= 0:
+        mA.run_timed(time_sp=(10000 + timemin) / 2.5, speed_sp=100)
+        sleep((10 + timemin / 1000) / 2.5)
+    else:
+        mA.run_timed(time_sp=4000, speed_sp=100)
+        sleep(4)
+        mD.run_timed(time_sp=timemin / 2.63, speed_sp=-100)
+        sleep(timemin / 1000)
+
+    # Move the robot until the distance is greater than or equal to 4.5 cm
+    while us.distance_centimeters >= 4.5:
+        mA.run_timed(time_sp=1000, speed_sp=-200)
+        mD.run_timed(time_sp=1000, speed_sp=-200)
+        print("Distance : ", us.distance_centimeters)
+    grabber.down()
+
+detect_can2()
 
 '''No line detected :
 Go at low speed in a right turn spiral forever'''
-def look_for_line() :
+
+
+def look_for_line():
     t = 1
     sp = SpeedPercent(20)
-    while() : #loop forever
+    while ():  # loop forever
         motors.on_for_seconds(t)
-        motors.turn_right(speed = sp, degrees=90)
+        motors.turn_right(speed=sp, degrees=90)
         t += 1
 
-def follow_line() :
+
+def follow_line():
     pass
